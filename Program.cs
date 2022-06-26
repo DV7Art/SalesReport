c6da4bbb7c59ddbc8a224e72724ae17b8abee68a
DV7Art <dvart@hotmail.com>
2022-06-26 18:55:48 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesReport
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal plan;
+            {
+                Console.Write("Введите план продаж: ");
+                plan = Convert.ToDecimal(Console.ReadLine());
+            }
+
+        }
+    }
+}
